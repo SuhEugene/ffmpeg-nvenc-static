add362530d12065bf0f45079e074f849405b20ac
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2026-08-06 13:53:06 +0300

    I apologize for even trying

--- build.sh
+++ build.sh
@@ -50,9 +50,7 @@ apt-get install -y \
     gsfonts \
     wget \
     libzmq5 \
-    libzmq3-dev
-
-apt-get install -y \
+    libzmq3-dev \
     libgnutls28-dev \
     libsdl2-dev \
     libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev \
@@ -72,104 +70,29 @@ wget https://ffmpeg.org/releases/ffmpeg-$1.tar.xz \
 #
 mkdir -p /tmp/bin
 # Configure and build ffmpeg with nvenc support
-#   ./configure --prefix=/usr/local \
-#     --bindir="/tmp/bin" \
-#     --enable-filter=drawtext \
-#     --enable-nonfree \
-#     --enable-nvenc \
-#     --enable-gpl \
-#     --enable-version3 \
-#     --enable-static \
-#     --disable-debug \
-#     --disable-ffplay \
-#     --disable-indev=sndio \
-#     --disable-outdev=sndio \
-#     --cc=gcc \
-#     --enable-fontconfig \
-#     --enable-gray \
-#     --enable-libmp3lame \
-#     --enable-libopus \
-#     --enable-libvpx \
-#     --enable-libx264 \
-#     --enable-libzmq \
-#     --disable-htmlpages \
-#     --extra-cflags=-I/usr/local/cuda/include \
-#     --extra-ldflags=-L/usr/local/cuda/lib64 \
+  
 cd ffmpeg-$1 \
- && ./configure --prefix=/usr \
+ && ./configure --prefix=/usr/local \
     --bindir="/tmp/bin" \
-    --libdir=/usr/lib/x86_64-linux-gnu \
-    --arch=amd64 \
+    --enable-filter=drawtext \
+    --enable-nonfree \
+    --enable-nvenc \
     --enable-gpl \
-    --disable-stripping \
-    --disable-omx \
-    --enable-gnutls \
-    --enable-libaom \
-    --enable-libass \
-    --enable-libbs2b \
-    --enable-libcaca \
-    --enable-libcdio \
-    --enable-libcodec2 \
-    --enable-libdav1d \
-    --enable-libflite \
-    --enable-libfontconfig \
-    --enable-libfreetype \
-    --enable-libfribidi \
-    --enable-libglslang \
-    --enable-libgme \
-    --enable-libgsm \
-    --enable-libharfbuzz \
+    --enable-version3 \
+    --enable-static \
+    --disable-debug \
+    --disable-ffplay \
+    --disable-indev=sndio \
+    --disable-outdev=sndio \
+    --cc=gcc \
+    --enable-fontconfig \
+    --enable-gray \
     --enable-libmp3lame \
-    --enable-libmysofa \
-    --enable-libopenjpeg \
-    --enable-libopenmpt \
     --enable-libopus \
-    --enable-librubberband \
-    --enable-libshine \
-    --enable-libsnappy \
-    --enable-libsoxr \
-    --enable-libspeex \
-    --enable-libtheora \
-    --enable-libtwolame \
-    --enable-libvidstab \
-    --enable-libvorbis \
     --enable-libvpx \
-    --enable-libwebp \
-    --enable-libx265 \
-    --enable-libxml2 \
-    --enable-libxvid \
-    --enable-libzimg \
-    --enable-openal \
-    --enable-opencl \
-    --enable-opengl \
-    --disable-sndio \
-    --enable-libvpl \
-    --disable-libmfx \
-    --enable-libdc1394 \
-    --enable-libdrm \
-    --enable-libiec61883 \
-    --enable-chromaprint \
-    --enable-frei0r \
-    --enable-ladspa \
-    --enable-libbluray \
-    --enable-libjack \
-    --enable-libpulse \
-    --enable-librabbitmq \
-    --enable-librist \
-    --enable-libsrt \
-    --enable-libssh \
-    --enable-libsvtav1 \
     --enable-libx264 \
     --enable-libzmq \
-    --enable-libzvbi \
-    --enable-lv2 \
-    --enable-sdl2 \
-    --enable-libplacebo \
-    --enable-librav1e \
-    --enable-pocketsphinx \
-    --enable-librsvg \
-    --enable-libjxl \
-    --enable-shared \
+    --enable-libvorbis \
     --disable-htmlpages \
     --extra-cflags=-I/usr/local/cuda/include \
     --extra-ldflags=-L/usr/local/cuda/lib64 \
